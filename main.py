c0caafee235caf794d8edcc75af44ae7586a2734
andreea <neguraandreea23@gmail.com>
2026-04-13 09:47:12 +0300

    first commit

---
+++ main.py
@@ -0,0 +1,24 @@
+import os
+import telebot
+
+
+BOT_TOKEN = os.environ.get("BOT_TOKEN")
+bot = telebot.TeleBot(BOT_TOKEN)
+
+
+@bot.message_handler(commands = ["start"])
+def start(message):
+    text = "Bot is active"
+    bot.reply_to(message, text)
+
+@bot.message_handler(content_types=["voice"])
+
+
+@bot.callback_query_handler(func=lambda call: True)
+def func():
+
+
+
+
+if __name__ == "__main__":
+    bot.polling(True)
